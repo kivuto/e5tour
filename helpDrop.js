685b4a2ea14e25b74a0d877a0392067e350505b8
U-E-ACADEMY\bwhite <bwhite@KVT-007.e-academy.local>
2014-05-07 12:23:17 -0400

    Adding four files required for tour. Run helpDrop.js to start tour.

---
+++ helpDrop.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+		var tourText = document.createElement("script"); tourText.type = "text/javascript"; tourText.src = "./js/indexIntro.js"; /*"https://raw.github.com/kivuto/e5Helpers/master/indexIntro.js";*/ $("head").append(tourText);
+		var tourScript = document.createElement("script"); tourScript.type = "text/javascript"; tourScript.src = "./js/intro.js";/*"https://raw.github.com/kivuto/e5Helpers/master/intro.js";*/ $("head").append(tourScript);
+		var tourCSS = document.createElement("link"); tourCSS.rel="stylesheet"; tourCSS.type = "text/css"; tourCSS.href = "./js/introjs.css"; /*"https://raw.github.com/kivuto/e5Helpers/master/introjs.css";*/ $("head").append(tourCSS);
+
+		var active = sessionStorage.isTour;
+
+		if($('#helpbutton').length > 0)
+		{							
+			$('li > #helpbutton').append("<li><span><a style='cursor:pointer' onclick='indexTourStart(false)'>Tour</a></span></li>");
+		}
+
+		if(active == "true")
+		{	
+			indexTourStart(true);
+		}
+});
