45d6683dabb0a126babbbcfcd6db419837a114f3
breconwhite <breconwhite@hotmail.com>
2014-05-08 15:55:04 -0400

    Update helpDrop.js

--- helpDrop.js
+++ helpDrop.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-		var tourText = document.createElement("script"); tourText.type = "text/javascript"; tourText.src = /*"indexIntro.js"; */"https://raw.github.com/kivuto/e5Helpers/master/indexIntro.js"; $("head").append(tourText);
-		var tourScript = document.createElement("script"); tourScript.type = "text/javascript"; tourScript.src = /*"intro.js";*/"https://raw.github.com/kivuto/e5Helpers/master/intro.js"; $("head").append(tourScript);
-		var tourCSS = document.createElement("link"); tourCSS.rel="stylesheet"; tourCSS.type = "text/css"; tourCSS.href = /*"introjs.css";*/ "https://raw.github.com/kivuto/e5Helpers/master/introjs.css"; $("head").append(tourCSS);
+		var tourText = document.createElement("script"); tourText.type = "text/javascript"; tourText.src = /*"indexIntro.js"; */"https://raw.github.com/kivuto/e5tour/master/indexIntro.js"; $("head").append(tourText);
+		var tourScript = document.createElement("script"); tourScript.type = "text/javascript"; tourScript.src = /*"intro.js";*/"https://raw.github.com/kivuto/e5tour/master/intro.js"; $("head").append(tourScript);
+		var tourCSS = document.createElement("link"); tourCSS.rel="stylesheet"; tourCSS.type = "text/css"; tourCSS.href = /*"introjs.css";*/ "https://raw.github.com/kivuto/e5tour/master/introjs.css"; $("head").append(tourCSS);
 
 		var active = sessionStorage.isTour;
 
